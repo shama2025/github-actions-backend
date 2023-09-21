e2a283482712f4ad46c63e8f2ece4b2aa15bd2ad
shama2025 <2003mashaffer@gmail.com>
2023-09-21 10:31:14 -0400

    Created pageOne.html
    I created this html file so I can see changes I made statically before I commit and push to main

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
 #This app.py file will contain the backend for the dynamic web applications
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def helloWorld():
+    return "<p> Hi there from page 1</p>"
