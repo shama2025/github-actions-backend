e6581fad3356097c3f2d2f57b426b85055b3b878
shama2025 <2003mashaffer@gmail.com>
2023-09-21 14:20:36 -0400

    Created navbar dynamically
    Dynamically created a navbar to rout to each page easier

---
+++ static/app.js
@@ -0,0 +1,7 @@
+let navBar = document.getElementById('navBar')
+navBar.innerHTML = `<ul>
+<li><a href="/pageOne">Page One</a></li>
+<li><a href="/pageTwo">Page Two</a></li>
+<li><a href="/pageThree">Page Three</a></li>
+</ul>`
+    
