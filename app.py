3586837c419319d28bac2d53f2abddd3c8f050b9
shama2025 <2003mashaffer@gmail.com>
2023-09-21 14:29:26 -0400

    Added navBar.html and made changes using pylint
    Used pylint feedback to edit and modify code

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# This app.py file will contain the backend for the dynamic web applications
+"""This app.py file will contain the backend for the dynamic web applications"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -6,9 +6,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def nav_bar():
+    """Will display the navbar.html file"""
     return render_template("navBar.html")
 
 
 @app.route("/pageOne")
 def page_one():
+    """Will Display pageOne.html"""
     return render_template("pageOne.html")
