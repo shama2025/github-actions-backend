167f60b68e6dc41c7fb99862d9d4e13b94a24af4
shama2025 <2003mashaffer@gmail.com>
2023-09-25 11:21:12 -0400

    Updated test_webtests.py

--- test_webtests.py
+++ test_webtests.py
@@ -1,10 +1,28 @@
-import pytest
-from flask import Flask
-from webapp import app
+"""This file will house the testing for the web applications"""
+from app import app
 
+
+def test_home_page(client):
+    """Will test the home page of the application"""
+    response = client.get("/")
+    assert b"<h1>Hello World</h1>" in response.data
 
 def test_page_one(client):
+    """Will test page one of my application"""
     response = client.get("/pageOne")
-    assert b" <h3>Welcome to Page three!</h3>" in response.data
+    assert b"<h1>Hello World</h1>" in response.data
 
+def test_page_two(client):
+    """Will test page one of my application"""
+    response = client.get("/pageTwo")
+    assert b"<h1>Hello World</h1>" in response.data
+
+def test_page_three(client):
+    """Will test page one of my application"""
+    response = client.get("/pageThree")
+    assert b"<h1>Hello World</h1>" in response.data
+
+test_home_page(app.test_client())
 test_page_one(app.test_client())
+test_page_two(app.test_client())
+test_page_three(app.test_client())
