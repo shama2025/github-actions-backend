6a5b6a61e2586333f5d26731497fa1a5d2c82be2
shama2025 <2003mashaffer@gmail.com>
2023-09-25 16:41:26 -0400

    Created webtesting with pytest and playwright
    Created a way to test the basic functionality of each web page. In addition to that, the workflows page was updated to account for pytest and installing playwright

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 """This app.py file will contain the backend for the dynamic web applications"""
 from flask import Flask, render_template
-import pytest
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,7 +7,7 @@ def nav_bar():
     """Will display the navbar.html file"""
     return render_template("navBar.html")
 
-@app.route("/pageOne")  
+@app.route("/pageOne")
 def page_one():
     """Will Display pageOne.html"""
     return render_template("pageOne.html")
--- test_webtests.py
+++ test_webtests.py
@@ -1,28 +1,29 @@
 """This file will house the testing for the web applications"""
-from app import app
+from playwright.sync_api import Page,expect
+import re
+def test_home_page(page:Page):
+    """Will test the navBar.html web app"""
+    page.goto("http://localhost:5000/")
+    page.get_by_role("link", name="Page One").click()
+    page.get_by_role("link", name="Page Two").click()
+    page.get_by_role("link", name="Page Three").click()
 
+def test_page_one(page:Page):
+    """Will test the pageOne.html web app"""
+    page.goto("http://localhost:5000/pageOne")
+    i = 0
+    while(i != 11 ):
+         page.get_by_role("button", name="Click Me!").click()
+         i+=1
+    page.get_by_role("button", name="Close").click()
 
-def test_home_page(client):
-    """Will test the home page of the application"""
-    response = client.get("/")
-    assert b"<h1>Hello World</h1>" in response.data
+def test_page_two(page:Page): #Get this to check the inner html
+    page.goto("http://localhost:5000/pageTwo")
+    page.get_by_placeholder("Enter text ").click()
+    page.get_by_placeholder("Enter text ").fill(".-/-.../..-/...-")
+    page.get_by_placeholder("Enter text ").press("Enter")
+    expect(page.locator(".translateText")).to_be_visible()
 
-def test_page_one(client):
-    """Will test page one of my application"""
-    response = client.get("/pageOne")
-    assert b"<h1>Hello World</h1>" in response.data
-
-def test_page_two(client):
-    """Will test page one of my application"""
-    response = client.get("/pageTwo")
-    assert b"<h1>Hello World</h1>" in response.data
-
-def test_page_three(client):
-    """Will test page one of my application"""
-    response = client.get("/pageThree")
-    assert b"<h1>Hello World</h1>" in response.data
-
-test_home_page(app.test_client())
-test_page_one(app.test_client())
-test_page_two(app.test_client())
-test_page_three(app.test_client())
+def test_page_three(page:Page):
+    page.goto("http://localhost:5000/pageThree")
+    expect(page.locator(".loading-text")).to_have_text("Loading...")
