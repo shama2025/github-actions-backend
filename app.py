6fd3943958378ab9caefcc46936b1b96590f3879
shama2025 <2003mashaffer@gmail.com>
2023-09-25 16:56:05 -0400

    Ran python -m black on all .py files
    python -m black was added to github flows before linter

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
 """This app.py file will contain the backend for the dynamic web applications"""
 from flask import Flask, render_template
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def nav_bar():
     """Will display the navbar.html file"""
     return render_template("navBar.html")
 
+
 @app.route("/pageOne")
 def page_one():
     """Will Display pageOne.html"""
     return render_template("pageOne.html")
 
+
 @app.route("/pageTwo")
 def page_two():
     """Will display pageTwo.html"""
     return render_template("pageTwo.html")
+
 
 @app.route("/pageThree")
 def page_three():
