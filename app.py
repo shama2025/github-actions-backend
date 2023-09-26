fb614a0e14b91060407208ff9918bfb1dd2fabcb
shama2025 <2003mashaffer@gmail.com>
2023-09-26 11:32:33 -0400

    Fixing web apps to be server side dynamic
    Making sure apps will always be different upon loading

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 """This app.py file will contain the backend for the dynamic web applications"""
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
@@ -15,11 +15,16 @@ def page_one():
     """Will Display pageOne.html"""
     return render_template("pageOne.html")
 
-
 @app.route("/pageTwo")
 def page_two():
     """Will display pageTwo.html"""
     return render_template("pageTwo.html")
+
+@app.route('/pageTwo/submit', methods=['POST'])
+def page_two_submit():
+    """This will grab a users form submition and return it"""
+    user_form_input = request.form.get("user_input")
+    return f"<h1>Hello user: {user_form_input}</h1>"
 
 @app.route("/pageThree")
 def page_three():
