83b093cec87e21b0a2cff0001893acbcaf6f5014
shama2025 <2003mashaffer@gmail.com>
2023-09-26 20:24:16 -0400

    Updated tests

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 """This app.py file will contain the backend for the dynamic web applications"""
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,26 +9,31 @@ def nav_bar():
     """Will display the navbar.html file"""
     return render_template("navBar.html")
 
+
 @app.route("/pageOne")
 def page_one():
     """Will Display pageOne.html"""
     return render_template("pageOne.html")
+
 
 @app.route("/pageTwo")
 def page_two():
     """Will displaye pageTwo.html"""
     return render_template("pageTwo.html")
 
-@app.route("/pageTwo/submit", methods=['POST'])
+
+@app.route("/pageTwo/submit", methods=["POST"])
 def page_two_submit():
     """This is the submit page where it renders the users input"""
     user_input = request.get_data("user_input")
-    return render_template("navBar.html")+ f"Your input was: {user_input}" 
+    return render_template("navBar.html") + f"Your input was: {user_input}"
+
 
 @app.route("/pageThree")
 def page_three():
     """Will display pageThree.html"""
     return render_template("pageThree.html")
+
 
 @app.route("/pageThree/class")
 def page_three_student():
--- test_webtests.py
+++ test_webtests.py
@@ -1,6 +1,7 @@
 """This file will house the testing for the web applications"""
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
 import datetime
+
 def test_home_page(page: Page):
     """Will test the navBar.html and check each of the pages"""
     page.goto("http://localhost:5000/")
@@ -13,9 +14,10 @@ def test_page_one(page: Page):
     """Will click the button ten times to show popup and then press cancel"""
     date = datetime.datetime.now()
     page.goto("http://localhost:5000/pageOne")
-    bool(page.get_by_text("Tue Sep 26 2023 19:45:20 GMT-0400 (Eastern Daylight Time)") == date)
-    
-    
+    bool(
+        page.get_by_text("Tue Sep 26 2023 19:45:20 GMT-0400 (Eastern Daylight Time)")
+        == date
+    )
 
 
 def test_page_two(page: Page):
@@ -23,9 +25,9 @@ def test_page_two(page: Page):
     page.goto("http://localhost:5000/pageTwo")
     page.get_by_label("Enter something:").click()
     page.get_by_label("Enter something:").fill("ABCDEFG")
+    user_input = page.locator("user_input").input_value
     page.get_by_role("button", name="Submit").click()
-    page.get_by_text("Page One Page Two Page Three Welcome to my WebApp! Use the navigation bar to go ").click()
-
+    bool(page.text_content == user_input)
 
 
 def test_page_three_student(page: Page):
@@ -37,9 +39,11 @@ def test_page_three_student(page: Page):
     page.get_by_placeholder("Password...").fill("password")
     page.get_by_label("Student:").check()
     page.get_by_role("button", name="Submit login form!").click()
-    page.get_by_role("cell", name="Dud Class One").click()
+    Ishidden = page.get_by_role("div, id=class_input").is_visible
+    bool(not Ishidden)
 
-def test_page_three_faculty(page:Page):
+
+def test_page_three_faculty(page: Page):
     """Will test the faculty side of the web page"""
     page.goto("http://localhost:5000/pageThree")
     page.get_by_placeholder("Username...").click()
@@ -51,4 +55,5 @@ def test_page_three_faculty(page:Page):
     page.get_by_placeholder("Add Class Name...").click()
     page.get_by_placeholder("Add Class Name...").fill("Dud Class two")
     page.get_by_role("button", name="Add Class!").click()
-    page.get_by_role("cell", name="Dud Class two").click()
+    Ishidden = page.get_by_role("div, id=class_input").is_visible
+    bool(Ishidden)
